6d739b9e7ac8a97e57cb66d70d00f9c32b3403c8
jgroman <jgroman@users.noreply.github.com>
2019-08-29 19:27:20 +0200

    Add initial code files

---
+++ example/lib_mlx90614_example/main.c
@@ -0,0 +1,27 @@
+﻿/***************************************************************************//**
+* @file    main.c
+* @version 1.0.0
+*
+* @brief .
+*
+* @author Jaroslav Groman
+*
+* @date
+*
+*******************************************************************************/
+
+#include "applibs_versions.h"   // API struct versions to use for applibs APIs
+#include <applibs/log.h>
+#include <applibs/i2c.h>
+
+// Import project hardware abstraction from project property 
+// "Target Hardware Definition Directory"
+#include <hw/project_hardware.h>
+
+#include "lib_mlx90614.h"
+
+int
+main(void)
+{
+    return 0;
+}
---
+++ lib_mlx90614/Inc/Public/lib_mlx90614.h
@@ -0,0 +1,26 @@
+/***************************************************************************//**
+* @file    lib_mlx90614.h
+* @version 1.0.0
+*
+* @brief .
+*
+* @author   Jaroslav Groman
+*
+* @date
+*
+*******************************************************************************/
+
+#ifndef _LIB_MLX90614_H_
+#define _LIB_MLX90614_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // _LIB_MLX90614_H_
+
+/* [] END OF FILE */
